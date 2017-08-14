e6b7e4c2b63cc2d9d525afa342dc01b068bfc3b1
Andy Little <kwikius@yahoo.com>
2017-08-14 17:03:06 +0100

    comments only

--- aerofoil.hpp
+++ aerofoil.hpp
@@ -8,17 +8,16 @@
 namespace quan{ namespace aero{
 
    /*
-      aerofoil is normalised so that le is at x= 0 and te is at y = 1.
+      Abstract Base Class
+      aerofoil is normalised so that le is at x= 0 and te is at x = 1.
       N.B that due to rounding errors some aerofoils may not have an exact chord of 1.
       Aerofoil consists of a number of 2d points starting at TE, going around upper 
       surface and then ending at te on lower surface
    */
 
-   struct aerofoil
-   {
+   struct aerofoil{
      virtual ~aerofoil(){}
      virtual std::string get_name() const = 0;
-     
      virtual std::size_t get_num_coords() const=0;
      virtual quan::two_d::vect<double> get_coord(int i) const=0;
      virtual quan::two_d::vect<double> get_upper_coord(double const & percent) const =0;
