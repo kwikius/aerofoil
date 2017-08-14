6194988e61698192bc821f9df93d525cee22df83
Andy Little <kwikius@yahoo.com>
2017-08-14 17:03:41 +0100

    comments and ws only

--- selig_aerofoil.hpp
+++ selig_aerofoil.hpp
@@ -8,10 +8,16 @@
 
 namespace quan{ namespace aero{
 
+   /*
+      An aerofoil consists of an upper surface and a lower surface which are connected at point(0,0) nominally
+      The x range is from 0 to 1
+      
+   */
+
    struct selig_aerofoil : aerofoil{
      
      bool load(std::string const & filename,std::ostream & e);
-        std::string  get_name() const ;
+        std::string get_name() const ;
         std::size_t get_num_coords() const;
         quan::two_d::vect<double> get_coord(int i) const;
 
@@ -23,8 +29,6 @@ namespace quan{ namespace aero{
      private:
          std::string m_name;
          std::vector<quan::two_d::vect<double> > m_coords;
-       
-
    };
 
 }}//quan:aero
